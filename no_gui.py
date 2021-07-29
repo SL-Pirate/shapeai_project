f113182c451822f15c3a7de592592e013e7b9cf2
SL-Pirate <80326180+SL-Pirate@users.noreply.github.com>
2021-07-29 16:52:41 +0530

    Update no_gui.py

--- no_gui.py
+++ no_gui.py
@@ -1,6 +1,8 @@
 import weather
 
+api = '857da17a4a663a880b6369b38ecd6501'
+
 city = input("Enter your location: ")
-get_weather = weather.Weather(city)
+get_weather = weather.Weather(city, api)
 result = get_weather.quick_run()
 print(result)
