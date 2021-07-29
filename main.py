e3976adf69b67bb74b14890e07f21eb928703da8
SL-Pirate <80326180+SL-Pirate@users.noreply.github.com>
2021-07-29 16:47:48 +0530

    made tiny changes to GUI
    Auto focus on entry box and Enter key can now run the program as well
    :)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import weather
 
+api = '857da17a4a663a880b6369b38ecd6501'
+
 root = tk.Tk()
 root.title('Weather')
 canvas = tk.Canvas(root, height=400, width=400)
@@ -17,11 +19,18 @@ out_label = tk.Label(root, bg='#ffffff')
 out_label.place(anchor='c', relwidth=0.8, relheight=0.4, relx=0.5, rely=0.725)
 
 def get_weather_init(location):
-    get_weather = weather.Weather(location)#, units='metric')
+    get_weather = weather.Weather(location, api)#, units='metric')
     output = get_weather.quick_run()
     out_label.config(text=output)
 
 run_button = tk.Button(root, text='Get weather', bg='#2AB0E7', font=('courier', 15), command=lambda:get_weather_init(get_entry.get()))
 run_button.place(anchor='c', relx=0.5, rely=0.4)
+
+def test(msg):
+    out_label.config(text=f'Worked\nmessage: {msg}')
+
+get_entry.bind('<Return>', lambda i:get_weather_init(get_entry.get()))
+
+get_entry.focus()
 
 root.mainloop()
